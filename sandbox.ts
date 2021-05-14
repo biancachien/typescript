0adcd4f3ba27c5c093dd3d3ed205e100a5af59b1
Bianca Chien <cocoamacaron@gmail.com>
2021-05-14 20:48:05 +0800

    Lesson2 & 3

--- sandbox.ts
+++ sandbox.ts
@@ -1,24 +1,32 @@
 //lesson2
 // const character = 'bianca';
+
 // console.log(character);
+
 // const inputs = document.querySelectorAll('input');
+
 // inputs.forEach(input => {
 //   console.log(input);
 // });
 ///////////////////////////////////////////////////////////////
 
 //lesson3
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
 // character = 20;
 character = 'luigi';
+
 // age = 'yoshi';
 age = 40;
+
 // isBlackBelt = 'yes';
 isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
 };
+
 // console.log(area('hello'));
 console.log(circ(7.5));
